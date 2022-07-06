34c8174e92fb4a985b9224fde05f4fc6fcee4104
Antonio David Villegas Yeguas <advy99@gmail.com>
2022-07-06 13:50:32 +0200

    math module - only with distance functions at this moment

---
+++ src/math/distance.rs
@@ -0,0 +1,22 @@
+pub fn minkowski_distance(point_a: &Vec<f64>, point_b: &Vec<f64>, p: i32) -> f64 {
+	let mut distance: f64 = 0.0;
+
+	let mut iterator = point_a.iter().zip(point_b.iter());
+
+	for (point_a_value, point_b_value) in iterator {
+		let abs_difference = (point_a_value - point_b_value).abs();
+		distance = distance + abs_difference.powi(p);
+	}
+
+	distance.powf(1.0/p)
+
+}
+
+
+pub fn euclidean_distance(point_a: &Vec<f64>, point_b: &Vec<f64>) -> f64 {
+	minkowski_distance(point_a, point_b, 2)
+}
+
+pub fn manhattan_distance(point_a: &Vec<f64>, point_b: &Vec<f64>) -> f64 {
+	minkowski_distance(point_a, point_b, 1)
+}
